4e2972d84bfd6b24daf31a84608546bb6ad252c9
Jonathan4991 <jonathanjoeblue@gmail.com>
2023-06-22 15:46:47 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,20 +1,13 @@
 #include "main.h"
 /**
-  *Return: Always 0 (Success)
-  */
-void positive_or_negative(int i)
+* main - Test function for positive or negative
+* Return: 0 (The while loop only)
+*/
+int main(void)
 {
-        if (i > 0)
-        {
-                printf("%d is positive\n", i);
-        }
-        else if (i == 0)
-        {
-                printf("%d is zero\n", i);
-        }
-        else
-        {
-                printf("%d is negative\n", i);
-        }
-
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
+
